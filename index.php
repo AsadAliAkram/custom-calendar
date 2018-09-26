236bbbe6402c2395507530ca1aa1bebb2ea4fcf7
Asad Ali Akram <asad.ali@devtrongenesis.com>
2018-09-26 13:37:46 +0500

    fix field width issue

--- index.php
+++ index.php
@@ -13,22 +13,19 @@
 	</div>
 	<!-- End Header Area -->
 
-	<div class="container">
-
 		<!-- Start Form Area -->
 		<div class="form">
 			<form action="display.php" method="post">
-				<label>Enter Day</label><br>
+				<span>Enter Day&nbsp&nbsp&nbsp&nbsp</span>
 				<input required type="number" name="day" min="1" max="22"><br><br>
-				<label>Enter Month</label><br>
+				<span>Enter Month</span>
 				<input required type="number" name="month" min="1" max="13"><br><br>
-				<label>Enter Year</label><br>
+				<span>Enter Year&nbsp&nbsp&nbsp</span>
 				<input required type="number" name="year" min="1"><br><br>
 				<input class="submit-button" type="submit" name="submit">
 			</form>
 		</div>
 		<!-- End Form Area -->
 
-	</div>
 </body>
 </html>
